cd9fa45987082e39c6014fd3cef9e40db8b76b13
Your Name <your.email@gmail.com>
2026-03-02 15:56:22 +0530

    ex10

---
+++ SST28-LLD101/SOLID/ex10/src/ConsoleUi.java
@@ -0,0 +1,5 @@
+public class ConsoleUi {
+    public void print(String s) {
+        System.out.println(s);
+    }
+}
---
+++ SST28-LLD101/SOLID/ex10/src/TransportBookingService.java
@@ -0,0 +1,30 @@
+public class TransportBookingService {
+    private final IDistanceCalculator distanceCalculator;
+    private final IDriverAllocator driverAllocator;
+    private final IPaymentGateway paymentGateway;
+
+    public TransportBookingService(IDistanceCalculator distanceCalculator,
+            IDriverAllocator driverAllocator,
+            IPaymentGateway paymentGateway) {
+        this.distanceCalculator = distanceCalculator;
+        this.driverAllocator = driverAllocator;
+        this.paymentGateway = paymentGateway;
+    }
+
+    public void book(TripRequest req) {
+        double km = distanceCalculator.km(req.from, req.to);
+        System.out.println("DistanceKm=" + km);
+
+        String driver = driverAllocator.allocate(req.studentId);
+        System.out.println("Driver=" + driver);
+
+        double fare = 50.0 + km * 6.6666666667;
+        fare = Math.round(fare * 100.0) / 100.0;
+
+        String txn = paymentGateway.charge(req.studentId, fare);
+        System.out.println("Payment=PAID txn=" + txn);
+
+        BookingReceipt r = new BookingReceipt("R-501", fare);
+        System.out.println("RECEIPT: " + r.id + " | fare=" + String.format("%.2f", r.fare));
+    }
+}
